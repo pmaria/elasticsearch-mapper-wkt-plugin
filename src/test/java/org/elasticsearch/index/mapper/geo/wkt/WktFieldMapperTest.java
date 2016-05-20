47e7f97b28ede00a0f11ca95106bb1f5fe32d6aa
Pano Maria <pano.maria@taxonic.com>
2016-05-20 11:58:27 +0200

    Extended tests
    Added more tests for WktFieldMapper. Still some scenarios to do.

--- src/test/java/org/elasticsearch/index/mapper/geo/wkt/WktFieldMapperTest.java
+++ src/test/java/org/elasticsearch/index/mapper/geo/wkt/WktFieldMapperTest.java
@@ -3,7 +3,11 @@ package org.elasticsearch.index.mapper.geo.wkt;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.common.geo.builders.ShapeBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.mapper.MapperParsingException;
 import org.elasticsearch.test.EsSingleWktMockNodeTestCase;
+import org.elasticsearch.test.geo.RandomShapeGenerator;
+
+import com.spatial4j.core.io.WKTWriter;
 
 import java.io.IOException;
 
@@ -12,15 +16,176 @@ import static org.elasticsearch.index.query.QueryBuilders.geoIntersectionQuery;
 import static org.elasticsearch.index.query.QueryBuilders.geoShapeQuery;
 import static org.elasticsearch.index.query.QueryBuilders.geoWithinQuery;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertSearchResponse;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
+/**
+ * Tests for the {@link WktFieldMapper}.
+ * TODO: Test indexed WKT shape; Test all geo mapping options
+ * 
+ * @author Pano Maria
+ *
+ */
 public class WktFieldMapperTest extends EsSingleWktMockNodeTestCase {
   
-    private void initWktSimple() throws IOException {
+    public void testEnvelopeQuery() throws IOException {
+        String[] index_type = initWktSimple();
+        final String index = index_type[0], type = index_type[1];
+        
+        client()
+        .prepareIndex(index, type, "1")
+        .setSource(jsonBuilder()
+                .startObject()
+                    .field("name", "Wind & Wetter, Berlin, Germany")
+                    .field("location", "POINT (13.400544 52.530286)")
+                .endObject()
+        )
+        .setRefresh(true)
+        .execute()
+        .actionGet();
+        
+        ShapeBuilder shape = ShapeBuilder.newEnvelope().topLeft(13.0, 53.0).bottomRight(14.0, 52.0);
+
+        SearchResponse searchResponse = client()
+                .prepareSearch(index)
+                .setTypes(type)
+                .setQuery(geoWithinQuery("location", shape))
+                .execute().actionGet();
+
+        assertSearchResponse(searchResponse);
+        assertThat(searchResponse.getHits().getTotalHits(), equalTo(1l));
+        assertThat(searchResponse.getHits().hits().length, equalTo(1));
+        assertThat(searchResponse.getHits().getAt(0).id(), equalTo("1"));
+    }
+
+    public void testIndexPointsFilterRectangle() throws Exception {
+        String[] index_type = initWktSimple();
+        final String index = index_type[0], type = index_type[1];
+        
+        client()
+        .prepareIndex(index, type, "1")
+        .setSource(jsonBuilder()
+                .startObject()
+                    .field("name", "Document1")
+                    .field("location", "POINT (-30 -30)")
+                .endObject()
+        )
+        .setRefresh(true)
+        .execute()
+        .actionGet();
+        
+        client()
+        .prepareIndex(index, type, "2")
+        .setSource(jsonBuilder()
+                .startObject()
+                    .field("name", "Document 2")
+                    .field("location", "POINT (-45 -50)")
+                .endObject()
+        )
+        .setRefresh(true)
+        .execute()
+        .actionGet();
+
+        ShapeBuilder shape = ShapeBuilder.newEnvelope().topLeft(-45, 45).bottomRight(45, -45);
+
+        SearchResponse searchResponse = client()
+                .prepareSearch(index)
+                .setTypes(type)
+                .setQuery(geoIntersectionQuery("location", shape))
+                .execute().actionGet();
+
+        assertSearchResponse(searchResponse);
+        assertThat(searchResponse.getHits().getTotalHits(), equalTo(1l));
+        assertThat(searchResponse.getHits().hits().length, equalTo(1));
+        assertThat(searchResponse.getHits().getAt(0).id(), equalTo("1"));
+
+        searchResponse = client()
+                .prepareSearch(index)
+                .setTypes(type)
+                .setQuery(geoShapeQuery("location", shape))
+                .execute()
+                .actionGet();
+
+        assertSearchResponse(searchResponse);
+        assertThat(searchResponse.getHits().getTotalHits(), equalTo(1l));
+        assertThat(searchResponse.getHits().hits().length, equalTo(1));
+        assertThat(searchResponse.getHits().getAt(0).id(), equalTo("1"));
+    }
+    
+    public void testEdgeCases() throws Exception {
+        String[] index_type = initWktQuadTree();
+        final String index = index_type[0], type = index_type[1];
+        
+        client()
+        .prepareIndex(index, type, "blakely")
+        .setSource(jsonBuilder()
+                .startObject()
+                    .field("name", "Blakely Island")
+                    .field("location", "POLYGON ((-122.83 48.57, -122.77 48.56, -122.79 48.53, -122.83 48.57))")
+                .endObject()
+        )
+        .setRefresh(true)
+        .execute()
+        .actionGet();
+
+        ShapeBuilder query = ShapeBuilder.newEnvelope().topLeft(-122.88, 48.62).bottomRight(-122.82, 48.54);
+
+        // This search would fail if both geoshape indexing and geoshape filtering
+        // used the bottom-level optimization in SpatialPrefixTree#recursiveGetNodes.
+        SearchResponse searchResponse = client()
+                .prepareSearch(index)
+                .setTypes(type)
+                .setQuery(geoIntersectionQuery("location", query))
+                .execute()
+                .actionGet();
+
+        assertSearchResponse(searchResponse);
+        assertThat(searchResponse.getHits().getTotalHits(), equalTo(1l));
+        assertThat(searchResponse.getHits().hits().length, equalTo(1));
+        assertThat(searchResponse.getHits().getAt(0).id(), equalTo("blakely"));
+    }
+    
+    public void testPointsOnly() throws Exception {
+        String[] index_type = initWktComplexPointsOnly();
+        final String index = index_type[0], type = index_type[1];
+
+        ShapeBuilder shape = RandomShapeGenerator.createShape(random());
+        String wktShape = new WKTWriter().toString(shape.build());
+        
+        try {
+            client()
+            .prepareIndex(index, type, "1")
+            .setSource(jsonBuilder()
+                    .startObject()
+                        .field("name", "Document1")
+                        .field("location", wktShape)
+                    .endObject()
+            )
+            .setRefresh(true).execute().actionGet();
+        } catch (MapperParsingException e) {
+            // RandomShapeGenerator created something other than a POINT type, verify the correct exception is thrown
+            assertThat(e.getCause().getMessage(), containsString("is configured for points only"));
+            return;
+        }
+
+        // test that point was inserted
+        SearchResponse response = client()
+                .prepareSearch(index)
+                .setTypes(type)
+                .setQuery(geoIntersectionQuery("location", shape))
+                .execute()
+                .actionGet();
+
+        assertEquals(1, response.getHits().getTotalHits());
+    }
+    
+    private String[] initWktSimple() throws IOException {
+        final String index = "my_index";
+        final String type = "my_type";
         
         String mapping = XContentFactory.jsonBuilder()
                 .startObject()
-                    .startObject("my_type")
+                    .startObject(type)
                         .startObject("properties")
                             .startObject("name")
                                 .field("type", "string")
@@ -36,106 +201,23 @@ public class WktFieldMapperTest extends EsSingleWktMockNodeTestCase {
         client()
         .admin()
         .indices()
-        .prepareCreate("my_index")
-        .addMapping("my_type", mapping)
+        .prepareCreate(index)
+        .addMapping(type, mapping)
         .execute()
         .actionGet();
         
         ensureGreen();
-    }
-   
-    public void testEnvelopeQuery() throws IOException {
-        initWktSimple();
         
-        client()
-        .prepareIndex("my_index", "my_type", "1")
-        .setSource(jsonBuilder()
-                .startObject()
-                    .field("name", "Wind & Wetter, Berlin, Germany")
-                    .field("location", "POINT (13.400544 52.530286)")
-                .endObject()
-        )
-        .setRefresh(true)
-        .execute()
-        .actionGet();
-        
-        ShapeBuilder shape = ShapeBuilder.newEnvelope().topLeft(13.0, 53.0).bottomRight(14.0, 52.0);
-        System.out.println(shape);
-
-        SearchResponse searchResponse = client()
-                .prepareSearch("my_index")
-                .setTypes("my_type")
-                .setQuery(geoWithinQuery("location", shape))
-                .execute().actionGet();
-
-        System.out.println(searchResponse);
-
-        assertSearchResponse(searchResponse);
-        assertThat(searchResponse.getHits().getTotalHits(), equalTo(1l));
-        assertThat(searchResponse.getHits().hits().length, equalTo(1));
-        assertThat(searchResponse.getHits().getAt(0).id(), equalTo("1"));
-    }
-
-    public void testIndexPointsFilterRectangle() throws Exception {
-        initWktSimple();
-        
-        client()
-        .prepareIndex("my_index", "my_type", "1")
-        .setSource(jsonBuilder()
-                .startObject()
-                    .field("name", "Document1")
-                    .field("location", "POINT (-30 -30)")
-                .endObject()
-        )
-        .setRefresh(true)
-        .execute()
-        .actionGet();
-        
-        client()
-        .prepareIndex("my_index", "my_type", "2")
-        .setSource(jsonBuilder()
-                .startObject()
-                    .field("name", "Document 2")
-                    .field("location", "POINT (-45 -50)")
-                .endObject()
-        )
-        .setRefresh(true)
-        .execute()
-        .actionGet();
-
-        ShapeBuilder shape = ShapeBuilder.newEnvelope().topLeft(-45, 45).bottomRight(45, -45);
-        System.out.println(shape);
-
-        SearchResponse searchResponse = client()
-                .prepareSearch("my_index")
-                .setTypes("my_type")
-                .setQuery(geoIntersectionQuery("location", shape))
-                .execute().actionGet();
-
-        System.out.println(searchResponse);
-
-        assertSearchResponse(searchResponse);
-        assertThat(searchResponse.getHits().getTotalHits(), equalTo(1l));
-        assertThat(searchResponse.getHits().hits().length, equalTo(1));
-        assertThat(searchResponse.getHits().getAt(0).id(), equalTo("1"));
-
-        searchResponse = client()
-                .prepareSearch("my_index")
-                .setTypes("my_type")
-                .setQuery(geoShapeQuery("location", shape))
-                .execute()
-                .actionGet();
-
-        assertSearchResponse(searchResponse);
-        assertThat(searchResponse.getHits().getTotalHits(), equalTo(1l));
-        assertThat(searchResponse.getHits().hits().length, equalTo(1));
-        assertThat(searchResponse.getHits().getAt(0).id(), equalTo("1"));
+        return new String[]{index, type};
     }
     
-    public void testEdgeCases() throws Exception {        
+    private String[] initWktQuadTree() throws IOException {
+        final String index = "my_index_2";
+        final String type = "my_type_2";
+        
         String mapping = XContentFactory.jsonBuilder()
                 .startObject()
-                    .startObject("my_type_2")
+                    .startObject(type)
                         .startObject("properties")
                             .startObject("name")
                                 .field("type", "string")
@@ -148,49 +230,53 @@ public class WktFieldMapperTest extends EsSingleWktMockNodeTestCase {
                     .endObject()
                 .endObject()
                 .string();
-        
-        System.out.println(mapping);
   
         client()
         .admin()
         .indices()
-        .prepareCreate("my_index_2")
-        .addMapping("my_type_2", mapping)
+        .prepareCreate(index)
+        .addMapping(type, mapping)
         .execute()
         .actionGet();
         
         ensureGreen();
-
-        client()
-        .prepareIndex("my_index_2", "my_type_2", "blakely")
-        .setSource(jsonBuilder()
+        
+        return new String[]{index, type};
+    }
+    
+    private String[] initWktComplexPointsOnly() throws IOException {
+        final String index = "geo_points_only";
+        final String type = "my_type_3";
+        
+        String mapping = XContentFactory.jsonBuilder()
                 .startObject()
-                    .field("name", "Blakely Island")
-                    .field("location", "POLYGON ((-122.83 48.57, -122.77 48.56, -122.79 48.53, -122.83 48.57))")
+                    .startObject(type)
+                        .startObject("properties")
+                            .startObject("name")
+                                .field("type", "string")
+                            .endObject()
+                            .startObject("location")
+                                .field("type", "wkt")
+                                .field("tree", randomBoolean() ? "quadtree" : "geohash")
+                                .field("tree_levels", "6")
+                                .field("distance_error_pct", "0.01")
+                                .field("points_only", true)
+                            .endObject()
+                        .endObject()
+                    .endObject()
                 .endObject()
-        )
-        .setRefresh(true)
+                .string();
+  
+        client()
+        .admin()
+        .indices()
+        .prepareCreate(index)
+        .addMapping(type, mapping)
         .execute()
         .actionGet();
-
-        ShapeBuilder query = ShapeBuilder.newEnvelope().topLeft(-122.88, 48.62).bottomRight(-122.82, 48.54);
         
-        System.out.println(query);
-
-        // This search would fail if both geoshape indexing and geoshape filtering
-        // used the bottom-level optimization in SpatialPrefixTree#recursiveGetNodes.
-        SearchResponse searchResponse = client()
-                .prepareSearch("my_index_2")
-                .setTypes("my_type_2")
-                .setQuery(geoIntersectionQuery("location", query))
-                .execute()
-                .actionGet();
+        ensureGreen();
         
-        System.out.println(searchResponse);
-
-        assertSearchResponse(searchResponse);
-        assertThat(searchResponse.getHits().getTotalHits(), equalTo(1l));
-        assertThat(searchResponse.getHits().hits().length, equalTo(1));
-        assertThat(searchResponse.getHits().getAt(0).id(), equalTo("blakely"));
+        return new String[]{index, type};
     }
 }
